480c3f6dadb12e3334f16fa943658a42f257dbce
Gabriel <fossecruor@gmail.com>
2024-03-25 16:59:48 +0100

    j'ai le demon

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,30 @@
+const studentHogwarts = () => {
+    let privateScore = 0;
+    let name = null;
+
+    function changeScoreBy(points) {
+        privateScore += points;
+    };
+    return {
+        setName: (newName) => name = newName,
+        rewardStudent: () => changeScoreBy(1),
+        penalizeStudent: () => changeScoreBy(-1),
+        getScore: () => console.log(`${name}: ${privateScore}`),
+    }
+}
+
+let harry = studentHogwarts();
+harry.setName("Harry");
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.getScore();
+
+let draco = studentHogwarts();
+draco.setName("Draco");
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.getScore();
