578440bd1d7a502796e58a11efea47080557e59b
Gabriel <fossecruor@gmail.com>
2024-03-26 09:41:35 +0100

    simple ok

---
+++ Javascript_advanced/12-room_area.js
@@ -0,0 +1,10 @@
+const roomDimensions = {
+    width: 50,
+    length: 100,
+    getArea() {
+        return this.width * this.length;
+    }
+}
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+boundGetArea();
